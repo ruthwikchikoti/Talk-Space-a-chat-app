4d376a3c2459510abfcd1cdd2972fd99a7196313
Ruthwik Chikoti <chikoti.23bcs10111@ms.sst.scaler.com>
2024-09-03 11:51:38 +0530

    enhanced chat

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -5,13 +5,21 @@ import { Avatar, IconButton } from '@mui/material';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-
-function Chat() {
-
+import InsertEmotionIcon from '@mui/icons-material/InsertEmoticon';
+import MicIcon from '@mui/icons-material/Mic';
+function Chat() { 
+  const [input, setInput] = useState('');
   const [seed, setSeed] = useState('');
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log('You typed >>>', input)
+    setInput('');
+  }
 
   return (
     <div className='chat'>
@@ -40,18 +48,29 @@ function Chat() {
       <p className={`chat__message ${true && 'chat__receiver'}`}>
             <span className='chat__name'>Ruthwik</span>
           Hey there
-            <span className='chat__timestamp'>3:52pm</span>
+            <span className='chat__timestamp'>3:52 pm</span>
 
           </p>
 
       </div>
 
       <div className='chat__footer'>
+        <IconButton>
+        <InsertEmotionIcon />
+        </IconButton>
+        <form>
+          <input  value={input}  onChange={e=> setInput(e.target.value)  } placeholder='Type a message' type='text' />
+          <button onClick={sendMessage} type='submit'>Send a message</button>
+        </form>
+        <IconButton>
+          <MicIcon />
+        </IconButton>
 
       </div>
 
     </div>
   )
+
 }
 
 export default Chat
